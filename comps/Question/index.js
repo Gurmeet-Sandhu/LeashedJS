256f7ecef15d95c9c39e854b876faed7d08b5255
milapdon <72219081+milapdon@users.noreply.github.com>
2020-10-29 11:26:03 -0700

    Done with the changes please double check

--- comps/Question/index.js
+++ comps/Question/index.js
@@ -11,14 +11,16 @@ border-radius: 30px;
 `;
 
 const Headertitle = styled.div`
-margin-left:30px;
-margin-top:30px;
+margin-left:25px;
+margin-top:25px;
 float:left;
+font-family: Poppins, Sans-serif;
+font-size:36px;
 `;
 
 const Closebutton = styled.div`
-margin-right:30px;
-margin-top:30px;
+margin-right:25px;
+margin-top:25px;
 float:right;
 `;
 
@@ -28,42 +30,78 @@ margin-left:45px;
 width:223px;
 float:center;
 text-align:center;
+font-family: Poppins, Sans-serif;
+font-style: medium;
+font-size:20px;
 `;
 
 const Truebutton = styled.div`
-margin-top:150px;
+margin-top:100px;
 margin-left:45px;
-width:223px;
+width:230px;
+height: 45px;
+border-radius: 193px;
+border: 4px solid #ff7158;
+fill: solid;
+background-color: #FF715B;
 float:center;
 text-align:center;
+justify-content: center;
+
 `;
 
 const Falsebutton = styled.div`
-margin-top:25px;
+margin-top:15px;
 margin-left:45px;
 width:223px;
 float:center;
 text-align:center;
+border: 4px solid #ff7158;
+border-radius: 193px;
+width:230px;
+height: 45px;
 `;
 
 const Pagenumber = styled.div`
-margin-top:50px;
+margin-top:15px;
 margin-left:45px;
 width:223px;
 float:center;
 text-align:center;
+font-family: Poppins, Sans-serif;
+font-style: SemiBold;
+font-size:18px;
+line-height: 100%;
 `;
 
-const QueBox = ({})=> {
+const Firstanswer = styled.div`
+justify-content: center;
+margin:10px;
+font-family: Poppins, Sans-serif;
+font-size:24px;
+color: #ffffff;
+`;
+
+const Secondanswer = styled.div`
+justify-content: center;
+margin:10px;
+font-family: Poppins, Sans-serif;
+font-size:24px;
+color: #ff7158;
+
+`;
+
+
+const QueBox = ({ Header1, Question1, Pagenumber1 })=> {
 
     return (
         <Queboxcontainer>
-            <Headertitle>Question #1</Headertitle>
+            <Headertitle>{Header1}</Headertitle>
             <Closebutton><img src="/close.png" /></Closebutton>
-            <Questiontext>Question text goes here Hi</Questiontext>
-            <Truebutton><img src="/truebutton.png" /></Truebutton>
-            <Falsebutton><img src="/falsebutton.png" /></Falsebutton>
-            <Pagenumber>1/5</Pagenumber>
+            <Questiontext>{Question1}</Questiontext>
+            <Truebutton><Firstanswer>True!</Firstanswer></Truebutton>
+            <Falsebutton><Secondanswer>False!</Secondanswer></Falsebutton>
+            <Pagenumber>{Pagenumber1}</Pagenumber>
         
         </Queboxcontainer>
     )
