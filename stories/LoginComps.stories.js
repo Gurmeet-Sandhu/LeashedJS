f88f42ed8ac52ab80aa655bbdc24bbe74ea3a5b7
milapdon <72219081+milapdon@users.noreply.github.com>
2020-11-03 13:47:01 -0800

    I'm pushing new comps

--- stories/LoginComps.stories.js
+++ stories/LoginComps.stories.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import LogBox from '../comps/Login';
 
- 
+
+
 export default {
     title: 'Leashed/Login Comps',
     };
 
+    export const LogSection = () => (
+        <LogBox 
+            
+        />
+      );
    
