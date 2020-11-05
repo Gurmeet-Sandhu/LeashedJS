e4751f46426aa96db3bed56524b9b5f018c48f1c
rockymalubay <rockymalubay@gmail.com>
2020-11-05 04:00:15 -0800

    Resource Structure

--- pages/Resources.js
+++ pages/Resources.js
@@ -1,6 +1,32 @@
 import React from 'react';
-
+import TopPart from '../comps/TopPart';
+import ResourceSub from '../comps/ResourceSub';
 
 export default function Resources() {
-    return 
+    return (
+      <div className="ResourceContainer">
+        <div className="TopBar">
+          <TopPart 
+           Pagename ="Resources"
+          />
+        </div>
+        <div className="ResourceSection">
+          <ResourceSub 
+            text=""
+          />
+          <ResourceSub 
+          
+          />
+          <ResourceSub 
+          
+          />
+          <ResourceSub 
+          
+          />
+          <ResourceSub 
+          
+          />
+        </div>
+      </div>
+    )
   }
