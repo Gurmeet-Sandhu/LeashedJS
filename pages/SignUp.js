6cc15e6e850199654c28ad06be87c4cc1d48a3e4
rockymalubay <rockymalubay@gmail.com>
2020-11-05 03:06:26 -0800

    Fixed Styling for Login

--- pages/SignUp.js
+++ pages/SignUp.js
@@ -1,6 +1,25 @@
 import React from 'react';
-
+import Signup from '../comps/SignupTitle';
+import SignupBox from '../comps/Signup';
 
 export default function Singup() {
-    return 
+    return (
+      <div className="SignupContainer">
+        <div className="Header">
+          <Signup 
+              title="Welcome!"
+              subtitle="Get the best from Leashed."
+          />
+        </div>
+        <div className="SignupSection">
+          <SignupBox 
+            LoginPart="Login"
+            SignupPart="Signup"
+          />
+        </div>
+        <div className="Logo">
+          <img src="/Horizontal Logo.png"></img>
+        </div>
+      </div>
+    )
   }
