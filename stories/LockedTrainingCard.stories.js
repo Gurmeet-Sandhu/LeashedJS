a5bbc4b9e9f65d7bf5159bf36141af9aa1d9a63c
mabeliang <mabeliang@hotmail.com>
2020-11-13 21:22:31 -0800

    Created the locked card component

---
+++ stories/LockedTrainingCard.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import LockedTrainingCard from '../comps/LockedTrainingCard';
+
+export default {
+    title:"Locked Training Card",
+    component: LockedTrainingCard
+};
+
+export const BasicLockedTrainingComp = () => <LockedTrainingCard />;
