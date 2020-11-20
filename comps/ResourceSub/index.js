a4151eb0640c2cd03e9f8a6b92f4302c848fdeff
= <=>
2020-11-19 20:45:42 -0800

    Created Resource text comp and added in the page

--- comps/ResourceSub/index.js
+++ comps/ResourceSub/index.js
@@ -5,12 +5,12 @@ const ResourceSubH = styled.div`
     max-width: fit-content;
     margin: 10%;
     padding:10px;
-    border:3px solid #000;
     border:${props=>props.Border ? props.Border : "5px solid #2F52E0"};
     border-radius:25px;
     display:flex;
     justify-content:center;
     align-items:center;
+    font-family: 'Mukta Mahee', Sans-serif;
 
     span {
         font-size:20px;
@@ -20,7 +20,7 @@ const ResourceSubH = styled.div`
 
 
 const ResourceSub = ({text,Border}) =>{
-    return <ResourceSubH border={Border}>
+    return <ResourceSubH style={{border:Border}}>
         <span>{text}</span>
     </ResourceSubH>
 }
