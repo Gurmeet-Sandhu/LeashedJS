29d10bf1b8153e69f1a1cdeac663ecd94fbbe52e
rockymalubay <rockymalubay@gmail.com>
2020-11-21 00:29:37 -0800

    Completed Timeline Page

---
+++ pages/Timeline.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import TopPart from '../comps/TopPart';
+import Timeline from '../comps/Timeline';
+import {useRouter} from 'next/router';
+
+export default function Timelines() {
+    const router = useRouter()
+    return (
+    <div className="Background"> 
+            <div className="TopBar"> 
+            <TopPart 
+                onClick={()=>{
+                    router.push('/Achievement')
+                }}
+                Pagename="Timeline"
+
+                icon={"/arrow_b_l.png"}
+                />
+            </div>
+            <div className="TimelineBox">
+                <Timeline
+                    Htext="Completed Lorem Ipsum"
+                    Subtext="Yesterday"
+                />
+                <Timeline
+                    Htext="Completed how to use treats."
+                    Subtext="Oct 21, 2020"
+                />
+                <Timeline
+                    Htext="Completed Pee & Poo Outside Day 2 "
+                    Subtext="Oct 21, 2020"
+                />
+                <Timeline
+                    Htext="Started Pee & Poo Outside Training."
+                    Subtext="Oct 19, 2020"
+                />
+                <Timeline
+                    Htext="Got Leashed."
+                    Subtext="Oct 16, 2020"
+                />
+            </div>
+    </div>
+    )
+  }
