bddca3b8adc327d6d40ee8cf8b21e5d62ebe744e
= <=>
2020-11-23 20:17:08 -0800

    router/link in menu comp and achive page+patternC

--- comps/GradientCard/index.js
+++ comps/GradientCard/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const GradientCardContainer = styled.div`
-    width: 368px;
+    width: 100%;
     height: 174px;
     background: linear-gradient(103.93deg, rgba(255, 113, 91, 0.63) 0%, rgba(249, 203, 64, 0.63) 98.7%, rgba(255, 255, 255, 0) 98.71%);
     border-radius: 30px;
     display:flex;
     align-items:center;
+    position:relative;
 `
 
 const GradientCardTitle = styled.div`
@@ -15,14 +16,16 @@ const GradientCardTitle = styled.div`
     font-weight:bold;
     max-width:200px;
     margin-left:35px;
+    color:#1B2121
 `
 const DayText = styled.div`
-    font-size: 18px;
+    font-size: 16px;
     font-style: normal;
     font-weight: 300;
-    text-align: right;
-    margin-top:120px;
-    margin-left:10px;
+    color:#1B2121;
+    position:absolute;
+    right:20px;
+    bottom:20px;    
 `
 
 const GradientCard = ({text, daytext}) => {
