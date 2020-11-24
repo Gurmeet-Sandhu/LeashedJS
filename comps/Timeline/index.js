1ca28f29dd1a65397119a733683f447e8a9fe777
= <=>
2020-11-23 21:48:30 -0800

    Timeline styling edited

--- comps/Timeline/index.js
+++ comps/Timeline/index.js
@@ -1,29 +1,46 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
+
+const Wrapper = styled.div`
+    Display: flex; 
+    flex-direction: column;
+    width: 80vw;
+    border-left:4px solid #2F52E0;
+    position:relative;
+    padding-left:20px;
+    justify-content:center;
+`;
+
 const Header = styled.div`
-    font-size: 22px; 
+    font-size: 20px; 
     font-weight: bold;
+    margin-top:10px;
 `;
 const Sub = styled.div`
     font-weight:lighter;
     font-size: 14px;
 `;
 
-const Wrapper = styled.div`
-    Display: flex; 
-    flex-direction: column;
-    width: 80vw;
-    margin: 5%;
+const Circle = styled.div`
+    position:absolute;
+    left:-12px;
+
+    img {
+        width:100%;
+        max-width:100%;
+        bottom:0;
+
+    }
 `;
-
-
 
 const Timeline= ({Htext, Subtext}) =>{
     return <Wrapper>
         <Header>{Htext}</Header>
-        <Sub> {Subtext} <br></br> <hr></hr></Sub>
-    
+        <Sub> {Subtext}</Sub>
+        <Circle><img src="/Circle_timeline.png"/></Circle>
+        <br/>
     </Wrapper>
 }
 
