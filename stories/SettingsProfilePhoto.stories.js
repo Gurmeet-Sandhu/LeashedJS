9dc77a6b3b31275f0d10b784ad4a83908d13497a
mabeliang <mabeliang@hotmail.com>
2020-11-24 16:04:47 -0800

    Created pfp and name comps for settings page

---
+++ stories/SettingsProfilePhoto.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import SettingsProfilePhoto from '../comps/SettingsProfilePhoto';
+
+export default {
+  title: 'Settings Profile Photo ',
+  component: SettingsProfilePhoto,
+};
+
+export const BasicProfilePhoto = () => <SettingsProfilePhoto />
